34eb7b1c24d208f21f6d5f57a0f09c6d8c2605ad
nanafranck <nanafranck94@gmail.com>
2024-11-26 01:44:42 +0100

    configuration environnement prod

---
+++ web-app/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+export const environment = {
+    production: true,
+
+    BACKEND_ROUTES: {
+        USER_API: 'api/users',
+        LOGIN_API_POST: '/login',
+        LOGOUT_API_GET: '/logout',
+        REGISTER_API_POST: '/register',
+        CURRENT_BOTTLES_POST: '/current-bottles'
+    },
+
+    FRONTEND_ROUTES:{
+        LOGIN: 'login',
+        REGISTER: 'register',
+        GESTION: 'gestion',
+        ERROR: 'error',
+        DASHBOARD: 'dashboard',
+        USERS: 'users',
+        BOUTEILES: 'bouteille',
+        RAPPORTS: 'rapport-sub',
+        PROFILE: 'profile'
+
+    },
+    
+    server_url: 'http://35.180.117.75:5000'
+  };
